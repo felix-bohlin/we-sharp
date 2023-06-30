986e8c43e08db40759a9413e2fe53bd97b5688b1
Felix <felix.bohlin@protonmail.com>
2023-06-30 16:28:26 +0200

    feat: card activity wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
-
 import solidJs from "@astrojs/solid-js";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
+  integrations: [solidJs(), image()]
 });
