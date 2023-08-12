be8280b138bfdf622dc6267e652767743a203015
Felix <felix.bohlin@protonmail.com>
2023-08-12 11:02:58 +0200

    fix: static scroll

--- src/components/nav/NavBottom.vue
+++ src/components/nav/NavBottom.vue
@@ -9,33 +9,34 @@ const uiStore = useUiStore()
   <Transition>
     <div v-if="!uiStore.modalModeActive" md:hidden sticky left-0 right-0 bottom-0 z-10 bg-main pb="[env(safe-area-inset-bottom)]" transition="padding 20">
       <nav
-        h-14 border="t zinc-700" grid grid-cols-5 gap-x-2 items-center text-xl of-y-scroll scrollbar-hide overscroll-none
+        h-14 border="t zinc-700" flex flex-row of-y-scroll scrollbar-hide overscroll-none
         class="after-content-empty after:h-[calc(100%+0.5px)] after:w-0.1px after:pointer-events-none"
       >
-        <!-- These weird styles above are used for scroll locking, don't change it unless you know exactly what you're doing. -->
-        <button grid place-items-center text-xs gap-1>
-          <Comment text-2xl />
-          Home
-        </button>
-        <button grid place-items-center text-xs gap-1>
-          <Comment text-2xl />
-          Stats
-        </button>
+        <div flex-1 grid grid-cols-5 gap-x-2 items-center text-xl>
+          <button grid place-items-center text-xs gap-1>
+            <Comment text-2xl />
+            Home
+          </button>
+          <button grid place-items-center text-xs gap-1>
+            <Comment text-2xl />
+            Stats
+          </button>
 
-        <button grid place-items-center text-xs gap-1>
-          <Comment text-2xl />
-          Add
-        </button>
+          <button grid place-items-center text-xs gap-1>
+            <Comment text-2xl />
+            Add
+          </button>
 
-        <button grid place-items-center text-xs gap-1>
-          <Comment text-2xl />
-          Group
-        </button>
+          <button grid place-items-center text-xs gap-1>
+            <Comment text-2xl />
+            Group
+          </button>
 
-        <button grid place-items-center text-xs gap-1>
-          <Comment text-2xl />
-          You
-        </button>
+          <button grid place-items-center text-xs gap-1>
+            <Comment text-2xl />
+            You
+          </button>
+        </div>
       </nav>
     </div>
   </Transition>
