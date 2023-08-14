93c77ab9a59a8a7d6f8c199968d6d54efeb40bc5
Felix <felix.bohlin@protonmail.com>
2023-08-14 12:39:12 +0200

    chore: add comments

--- uno.config.ts
+++ uno.config.ts
@@ -5,14 +5,18 @@ export default defineConfig({
   presets: [presetUno(), presetAttributify(), presetIcons()],
   transformers: [transformerDirectives()],
   shortcuts: {
+    // background
     'bg-main': 'bg-slate-50 @dark:bg-zinc-900',
     'bg-1': 'bg-white @dark:bg-zinc-800',
     'bg-2': 'bg-zinc-200 @dark:bg-zinc-400/10',
 
+    // text
     'text-main': 'text-zinc-800 @dark:text-zinc-200',
 
+    // border
     'border-color-1': 'border-zinc-200 @dark:border-zinc-400/10',
 
+    // ring
     'ring-color-1': 'ring-zinc-200 @dark:ring-zinc-400/10',
     'ring-color-1-hover': 'ring-zinc-800 @dark:ring-zinc-600',
   },
