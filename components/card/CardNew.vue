bd9495404cf38f663974c415bb22fca6addf8ff9
Felix <felix.bohlin@protonmail.com>
2023-09-04 13:37:57 +0200

    feat(cardnew): add modal and drawer

--- components/card/CardNew.vue
+++ components/card/CardNew.vue
@@ -5,11 +5,13 @@ defineProps<{
   title: string
   type: TActivityType
 }>()
+
+defineEmits(['onClick'])
 </script>
 
 <template>
   <Card p="y-6 " grid place-items-center rounded-xl hover:text-primary>
-    <button grid h-full w-full place-items-center>
+    <button grid h-full w-full place-items-center @click="$emit('onClick')">
       <div grid place-items-center gap-2>
         <span inline-flex text-4xl>
           <IconsActivity :icon="type" />
--- pages/add/index.vue
+++ pages/add/index.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts">
+import { breakpointsTailwind, useBreakpoints } from '@vueuse/core'
+import Dialog from '@/components/dialog/Dialog.vue'
+import type { TActivityType } from '~/types/activity'
+
 useHead({
   title: 'Add | WeSharp',
 })
+
+const dialog = ref<InstanceType<typeof Dialog>>()
+const showModal = ref(false)
+const activeType = ref<null | TActivityType>(null)
+
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const isDesktop = breakpoints.greaterOrEqual('sm')
+
+const uiStore = useUiStore()
+
+function modalShow(type: TActivityType) {
+  showModal.value = true
+  activeType.value = type
+
+  if (breakpoints.isGreaterOrEqual('sm'))
+    dialog.value?.show()
+
+  else
+    uiStore.toggleModalMode(true)
+}
+
+function modalCloseAll() {
+  activeType.value = null
+  showModal.value = false
+  dialog.value?.close()
+  uiStore.toggleModalMode(false)
+}
 </script>
 
 <template>
@@ -10,16 +41,28 @@ useHead({
       New activity
     </h1>
 
-    <div grid="~ cols-2  gap-2 md:cols-3 md:gap-4">
-      <CardNew title="Ballsport" type="ballsport" />
-      <CardNew title="Cycling" type="cycling" />
-      <CardNew title="Golf" type="golf" />
-      <CardNew title="Gym" type="gym" />
-      <CardNew title="Martial arts" type="martialarts" />
-      <CardNew title="Running" type="run" />
-      <CardNew title="Swimming" type="swim" />
-      <CardNew title="Walking" type="walk" />
-      <CardNew title="Yoga" type="yoga" />
+    <div grid="~ cols-2  gap-2 md:cols-3 md:gap-4" mt-6>
+      <CardNew title="Ballsport" type="ballsport" @on-click="modalShow('ballsport')" />
+      <CardNew title="Cycling" type="cycling" @on-click="modalShow('cycling')" />
+      <CardNew title="Golf" type="golf" @on-click="modalShow('golf')" />
+      <CardNew title="Gym" type="gym" @on-click="modalShow('gym')" />
+      <CardNew title="Martial arts" type="martialarts" @on-click="modalShow('martialarts')" />
+      <CardNew title="Running" type="run" @on-click="modalShow('run')" />
+      <CardNew title="Swimming" type="swim" @on-click="modalShow('swim')" />
+      <CardNew title="Walking" type="walk" @on-click="modalShow('walk')" />
+      <CardNew title="Yoga" type="yoga" @on-click="modalShow('yoga')" />
     </div>
   </div>
+
+  <Drawer :show-drawer="showModal && !isDesktop" title="New activity" @on-close="modalCloseAll()">
+    {{ activeType }}
+  </Drawer>
+
+  <Dialog ref="dialog" title="New activity" :show-close="true">
+    {{ activeType }}
+
+    <template #bottom>
+      bottom
+    </template>
+  </Dialog>
 </template>
