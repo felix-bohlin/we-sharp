77b5fd69fa9c73a90f2e2ea1b0019a72388b8ff1
Felix <felix.bohlin@protonmail.com>
2023-09-06 19:47:16 +0200

    feat: mood opacity

--- components/icons/Mood.vue
+++ components/icons/Mood.vue
@@ -7,9 +7,9 @@ defineProps<{
 </script>
 
 <template>
-  <span v-if="icon === 'terrible'" i-mdi-emoticon-dead class="text-red/40" />
-  <span v-else-if="icon === 'bad'" i-mdi-emoticon-confused class="text-yellow/40" />
+  <span v-if="icon === 'terrible'" i-mdi-emoticon-dead class="text-red" />
+  <span v-else-if="icon === 'bad'" i-mdi-emoticon-confused class="text-yellow" />
   <span v-else-if="icon === 'neutral'" i-mdi-emoticon-neutral class="text-secondary" />
-  <span v-else-if="icon === 'good'" i-mdi-emoticon-happy class="text-blue/40" />
-  <span v-else-if="icon === 'great'" i-mdi-emoticon-cool class="text-green/40" />
+  <span v-else-if="icon === 'good'" i-mdi-emoticon-happy class="text-blue" />
+  <span v-else-if="icon === 'great'" i-mdi-emoticon-cool class="text-green" />
 </template>
