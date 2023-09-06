1060325e2ce7654d1c7bb93fbc45f8000f327d62
Felix <felix.bohlin@protonmail.com>
2023-09-06 19:47:35 +0200

    feat: new card icon size

--- components/card/CardNew.vue
+++ components/card/CardNew.vue
@@ -13,7 +13,7 @@ defineEmits(['onClick'])
   <Card p="y-3 e-4 s-4 md:e-0 md:s-0 md:y-6" grid place-items-center rounded-xl hover:text-primary>
     <button grid h-full w-full md:place-items-center @click="$emit('onClick')">
       <div flex items-center gap-2 md:grid md:place-items-center>
-        <span inline-flex text-4xl>
+        <span inline-flex text-3xl md:text-4xl>
           <IconsActivity :icon="type" />
         </span>
         <span text-lg md:text-xl>{{ title }}</span>
